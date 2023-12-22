4b8e7044f674a21ae6fb620c312ccfda729ba906
Jared Elliott <bootcamp@jaredclt.com>
2023-12-22 13:47:30 -0500

    Added form handler and event listener for search. Outputs to console log.

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,3 +3,28 @@
 // q = search term
 // at = attributes | ex: pagination, results, results.0, facets
 
+const userSearchTerm = $("#library-search");
+const userSearchFormat = $("#library-format");
+const formSubmit = $('button[type=submit]');
+
+$(function() {
+    //Verifies that the page load has finished and jQuery is active
+
+
+    //Listening for the click on the form submit button
+    formSubmit.on("click", formSearchHandler);
+});
+
+var formSearchHandler = function (event) {
+    //Prevents page from refreshing
+    event.preventDefault();
+  
+    var searchTerm = userSearchTerm.val().trim();
+  
+    if (searchTerm) {
+        //If searchTerm is not null/blank, here is what we do.
+        console.log('formSearchHandler:', searchTerm)
+        const libraryQuery = 'https://www.loc.gov/search/?q=' + searchTerm + '&fo=json&at=results'
+        console.log('formSearchHandler:', libraryQuery)
+    } else { console.log('formSearchHandler:', 'Search Term was null'); }
+  };
