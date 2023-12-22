ac890b72917e995a86b2b4e7e5c2895bd39ab7da
Jared Elliott <bootcamp@jaredclt.com>
2023-12-22 13:26:24 -0500

    Added comments about the api call.

---
+++ assets/js/search.js
@@ -0,0 +1,4 @@
+// API Call Example: https://www.loc.gov/search/?q=baseball&fo=json&at=results
+// Returns results for the query baseball in json format
+// q = search term
+// at = attributes | ex: pagination, results, results.0, facets
